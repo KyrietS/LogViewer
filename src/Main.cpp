1405518c95f476dc7608389aa182798d4b27988e
Kyriet <kyriet.sebastian@gmail.com>
2023-12-10 20:11:42 +0100

    Configure FLTK library in CMake

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Window.H>
 
-using namespace std;
-
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    Fl::get_system_colors();
+
+    Fl_Window *window = new Fl_Window(340, 180);
+    Fl_Box *box = new Fl_Box(0, 0, 340, 120, "Hello, World!");
+    Fl_Button *button = new Fl_Button(120, 120, 100, 30, "Close");
+    button->callback([](Fl_Widget *w, void *) { w->window()->hide(); });
+    button->clear_visible_focus();
+
+    window->resizable(window);
+    window->end();
+    window->show();
+
+    return Fl::run();
 }
